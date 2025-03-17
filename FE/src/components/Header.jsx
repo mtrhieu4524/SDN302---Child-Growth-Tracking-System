84bd013f2ad01ba3e2803a4acd82a81cb8f94ae6
mtrhieu4524 <146727369+mtrhieu4524@users.noreply.github.com>
2025-03-17 23:08:42 +0700

    modify package management

--- FE/src/components/Header.jsx
+++ FE/src/components/Header.jsx
@@ -49,7 +49,10 @@ const HeaderComponent = () => {
       key: "health",
       label: "Health & Nutrition",
       children: [
-        { key: "growth-chart", label: <a href="/growth-chart">Growth Chart</a> },
+        {
+          key: "growth-chart",
+          label: <a href="/growth-chart">Growth Chart</a>,
+        },
         {
           key: "milestones",
           label: <a href="/development-milestones">Development Milestones</a>,
@@ -57,6 +60,7 @@ const HeaderComponent = () => {
       ],
     },
     { key: "blogs", label: <a href="/blogs">Blogs</a> },
+    { key: "membership", label: <a href="/membership">Membership</a> },
     { key: "faqs", label: <a href="/faqs">FAQs</a> },
     { key: "about-us", label: <a href="/about-us">About Us</a> },
   ];
@@ -90,7 +94,10 @@ const HeaderComponent = () => {
       key: "health",
       label: "Health & Nutrition",
       children: [
-        { key: "growth-chart", label: <a href="/growth-chart">Growth Chart</a> },
+        {
+          key: "growth-chart",
+          label: <a href="/growth-chart">Growth Chart</a>,
+        },
         {
           key: "milestones",
           label: <a href="/development-milestones">Development Milestones</a>,
@@ -136,7 +143,10 @@ const HeaderComponent = () => {
       key: "health",
       label: "Health & Nutrition",
       children: [
-        { key: "growth-chart", label: <a href="/growth-chart">Growth Chart</a> },
+        {
+          key: "growth-chart",
+          label: <a href="/growth-chart">Growth Chart</a>,
+        },
         {
           key: "milestones",
           label: <a href="/development-milestones">Development Milestones</a>,
@@ -153,15 +163,15 @@ const HeaderComponent = () => {
     role === "admin"
       ? adminMenuItems
       : role === "doctor"
-        ? doctorMenuItems
-        : memberMenuItems;
+      ? doctorMenuItems
+      : memberMenuItems;
 
   const dropdownItems =
     role === "admin"
       ? adminDropdownMenuItems
       : role === "doctor"
-        ? doctorDropdownMenuItems
-        : memberDropdownMenuItems;
+      ? doctorDropdownMenuItems
+      : memberDropdownMenuItems;
 
   return (
     <div>
@@ -178,8 +188,7 @@ const HeaderComponent = () => {
           marginRight: "-8px",
           paddingTop: "10px",
           marginBottom: "5px",
-        }}
-      >
+        }}>
         Welcome to Child Growth Tracking
       </div>
 
@@ -199,8 +208,7 @@ const HeaderComponent = () => {
           position: "sticky",
           top: "10px",
           zIndex: 50,
-        }}
-      >
+        }}>
         <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
           <a href="/" title="Logo">
             <h3
@@ -208,8 +216,7 @@ const HeaderComponent = () => {
                 marginTop: "25px",
                 marginLeft: "30px",
                 marginRight: "25px",
-              }}
-            >
+              }}>
               CHILD GROWTH TRACKING
             </h3>
           </a>
@@ -234,8 +241,7 @@ const HeaderComponent = () => {
             alignItems: "center",
             gap: "15px",
             marginRight: "10px",
-          }}
-        >
+          }}>
           {user ? (
             <Dropdown menu={{ items: dropdownItems }} placement="bottomRight">
               <a
@@ -244,8 +250,7 @@ const HeaderComponent = () => {
                   fontWeight: "500",
                   display: "flex",
                   alignItems: "center",
-                }}
-              >
+                }}>
                 <UserOutlined
                   style={{ marginRight: "5px", marginTop: "4px", fontSize: 24 }}
                 />
@@ -263,8 +268,7 @@ const HeaderComponent = () => {
               <Button
                 type="primary"
                 href="/register"
-                style={{ background: "#0082C8", borderColor: "#0082C8" }}
-              >
+                style={{ background: "#0082C8", borderColor: "#0082C8" }}>
                 Create account
               </Button>
             </>
